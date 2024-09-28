ad5890b7a66d92f2499fa728c7661555a4968250
PhungNghiaMa <126763513+PhungNghiaMa@users.noreply.github.com>
2024-09-29 00:58:50 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import 'foundation-sites/dist/css/foundation.min.css';
 import App from './App.jsx'
-// import './index.css'
 import './Frontend/Decorator.css'
-import 'react-notifications/lib/notifications.css';
 
 
 createRoot(document.getElementById('root')).render(
